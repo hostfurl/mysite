9ca40aa0de531f2d3ff647d10cc3582f8e341358
John Heenan <john@zgus.com>
2024-01-13 10:44:40 +1000

    updates

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/@hostfurl/astro-mailform/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
